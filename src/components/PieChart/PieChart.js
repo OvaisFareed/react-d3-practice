f5b00dd862d39eb0bf15b0efbb51130c85211aa2
OvaisFareed <movaissiddiqui16@gmail.com>
2020-07-07 01:34:19 +0500

    Update pie chart, added hover feature

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './PieChart.css';
 import * as d3 from 'd3';
+import RandomData from '../../constants/RandomData';
 
 function PieChart() {
     // set the dimensions and margins of the graph
@@ -15,22 +17,34 @@ function PieChart() {
     const width = w - margin.right - margin.left,
         height = h - margin.top - margin.bottom;
 
-        // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-        const radius = Math.min(width, height) / 2 - margin.top;
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    const radius = Math.min(width, height) / 2 - margin.top;
 
-        // Create dummy data
-        const data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
+    // Create dummy data
+    // const data = { a: 9, b: 20, c: 30, d: 8, e: 12 };
+    const { ringsData } = RandomData();
+    const data = {};
 
-        // set the color scale
-        const color = d3.scaleOrdinal()
-            .domain(data)
-            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+    ringsData.forEach((d, i) => {
+        data[i] = d.cx;
+    });
 
-        // Compute the position of each group on the pie:
-        const pie = d3.pie()
-            .value(function (d) { return d.value; });
+    // set the color scale
+    const color = d3.scaleOrdinal()
+        .domain(data)
+        .range(["blue", "orange", "red", "darkRed", "brown"])
 
-        const data_ready = pie(d3.entries(data));
+    // Compute the position of each group on the pie:
+    const pie = d3.pie()
+        .value((d) => d.value);
+
+    const data_ready = pie(d3.entries(data));
+
+    const createArc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+    const format = d3.format(".2f");
 
     return (
         <div>
@@ -45,8 +59,12 @@ function PieChart() {
                         <stop offset="100%" stopColor="#00ff00" />
                     </linearGradient>
                     <linearGradient id="custom" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="20%" stopColor="orange" />
+                        <stop offset="0%" stopColor="violet" />
+                        <stop offset="15%" stopColor="indigo" />
+                        <stop offset="30%" stopColor="blue" />
+                        <stop offset="45%" stopColor="green" />
+                        <stop offset="60%" stopColor="yellow" />
+                        <stop offset="75%" stopColor="orange" />
                         <stop offset="100%" stopColor="red" />
                     </linearGradient>
                 </defs>
@@ -54,14 +72,26 @@ function PieChart() {
                 <g transform={`translate(${w / 2},${h / 2})`}>
                     {data_ready.map((e, i) => (
                         // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-                        <path
-                            key={i}
-                            d={`${d3.arc().innerRadius(0).outerRadius(radius)(e)}`}
-                            fill={color(e.data.key)}
-                            stroke="black"
-                            strokeWidth="2px"
-                            opacity={0.7}
-                        ></path>
+                        <g key={i}>
+                            <path
+                                className="pie"
+                                d={createArc(e)}
+                                fill={color(e.data.key)}
+                                // fill={`url(#custom)`}
+                                strokeWidth="2px"
+                                opacity={0.7}
+                            ></path>
+
+                            <text
+                                transform={`translate(${createArc.centroid(e)})`}
+                                textAnchor="middle"
+                                alignmentBaseline="middle"
+                                fill="white"
+                                fontSize="14"
+                            >
+                                {format(e.value)}
+                            </text>
+                        </g>
                     ))}
                 </g>
             </svg>
